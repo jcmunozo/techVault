820609d815972cf2288963602a638ac1afb04ae3
jcmunozo <jcmunozo@unicauca.edu.co>
2024-08-06 22:17:48 -0500

    Added slug for creator

--- creators/models.py
+++ creators/models.py
@@ -9,6 +9,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 class Creator(models.Model):
     """Creators model"""
     name = models.CharField("Creator name",max_length=20)
+    slug = models.SlugField("slug", unique=True, max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField("Created", auto_now_add=True, blank=True)
     biography = CKEditor5Field("Biography")
