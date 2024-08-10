4b5878247814939464934a01db8f0211a84c2463
jcmunozo <jcmunozo@unicauca.edu.co>
2024-08-09 23:03:55 -0500

    Separated some html elements, for make it reuseable

--- techVault/views.py
+++ techVault/views.py
@@ -2,12 +2,17 @@
 # Django
 from django.shortcuts import render
 from django.views.generic import ListView
-from techs.models import Tech
 from django.db.models import Q
+
+# Tech
+from techs.models import Tech
+
+# Creator
+from creators.models import Creator
 
 class Home(ListView):
     model = Tech
-    template_name = 'home.html'
+    template_name = 'app/home.html'
     context_object_name = 'techs'
     paginate_by = 2
 
@@ -16,15 +21,24 @@ class Home(ListView):
         search_query = self.request.GET.get("search")
         
         if self.request.user.is_authenticated:
-            queryset = Tech.objects.filter(user=self.request.user)
+            tech_queryset = Tech.objects.filter(user=self.request.user)
         else:
             queryset = Tech.objects.filter(visibility=True)
         
         if search_query:
-            queryset = queryset.filter(
+            tech_queryset = queryset.filter(
                 Q(name__icontains=search_query) |
                 Q(description__icontains=search_query)
             ).distinct()
+
+            creator_queryset = Creator.objects.filter(
+                Q(name__icontains=search_query) |
+                Q(biography__icontains=search_query)
+            ).distinct()
+
+            combined_queryset = list(tech_queryset) + list(creator_queryset)
+            return combined_queryset
+
         return queryset
     
     def get_context_data(self, **kwargs):
@@ -33,4 +47,4 @@ class Home(ListView):
         return context
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'app/about.html')
